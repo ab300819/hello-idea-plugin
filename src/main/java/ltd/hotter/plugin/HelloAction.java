339e5bab7858eb9e385c634c5049906c583c2fb9
Mason <ab300819@163.com>
2022-10-20 10:12:13 +0800

    init commit

---
+++ src/main/java/ltd/hotter/plugin/HelloAction.java
@@ -0,0 +1,21 @@
+package ltd.hotter.plugin;
+
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * <p></p>
+ *
+ * @author mason
+ * @date 2022/10/19 22:30
+ */
+public class HelloAction extends AnAction {
+
+    
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
